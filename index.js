421ab353668038279ae2a535a9ec5ca08bd7e001
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-03 19:37:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var beatles = []
-  for (let i = 0; i < 5; i++){
-    beatles.unshift(musicians[i] + "plays" + instruments[i])
+  for (let i = 0; i < 4; i++){
+    beatles.unshift(musicians[i] + " plays " + instruments[i])
     
   }
   return beatles;
