0d2eb1b25b6c5a3797393860637e8d0290b372b8
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-03 18:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var beatles = []
   for (i = 0; i < 5; i++){
-    array.push(musician plays instrument)
+    array.push(musicians, instruments)
   }
   return beatles;
 }
