357cd82ce653af464eeda1021fdea8de0425f806
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-03 18:49:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var beatles = []
-  
+  for (i = 0; i < 5; i++){
+    array.push(${musician} plays ${instrument})
+  }
+  return beatles;
 }
